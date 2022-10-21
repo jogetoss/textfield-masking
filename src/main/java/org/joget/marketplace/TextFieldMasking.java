6836a7316108c1939a26123e09718a8e1f361f6e
Hugo Lim <70784722+hugoatjoget@users.noreply.github.com>
2022-10-21 22:35:26 +0800

    Initial commit

---
+++ src/main/java/org/joget/marketplace/TextFieldMasking.java
@@ -0,0 +1,92 @@
+package org.joget.marketplace;
+
+import java.util.Map;
+import org.joget.apps.app.service.AppUtil;
+import org.joget.apps.form.model.FormData;
+import org.joget.apps.form.service.FormUtil;
+import org.joget.apps.form.lib.TextField;
+import org.joget.commons.util.SecurityUtil;
+import org.joget.commons.util.StringUtil;
+
+public class TextFieldMasking extends TextField {
+
+    @Override
+    public String getName() {
+        return "Text Field Masking";
+    }
+
+    @Override
+    public String getVersion() {
+        return "7.0.0";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Text Field Masking";
+    }
+    
+    @Override
+    public String renderTemplate(FormData formData, Map dataModel) {
+        String template = "textFieldMasking.ftl";
+
+        // set value
+        String value = FormUtil.getElementPropertyValue(this, formData);
+        
+        value = SecurityUtil.decrypt(value);
+        
+        if (FormUtil.isReadonly(this, formData) && "true".equalsIgnoreCase(getPropertyString("readonlyLabel"))) {
+            String valueLabel = value;
+            if (!getPropertyString("style").isEmpty() && "true".equalsIgnoreCase(getPropertyString("storeNumeric"))) {
+                valueLabel = StringUtil.numberFormat(value, getPropertyString("style"), getPropertyString("prefix"), getPropertyString("postfix"), "true".equalsIgnoreCase(getPropertyString("useThousandSeparator")), getPropertyString("numOfDecimal"));
+            }
+            dataModel.put("valueLabel", valueLabel);
+        }
+        dataModel.put("value", value);
+        
+
+        String html = FormUtil.generateElementHtml(this, formData, template, dataModel);
+        return html;
+    }
+
+    @Override
+    public String getClassName() {
+        return getClass().getName();
+    }
+
+    @Override
+    public String getFormBuilderTemplate() {
+        return "<label class='label'>Text Field Masking</label>";
+    }
+
+    @Override
+    public String getLabel() {
+        return "Text Field Masking";
+    }
+    
+    @Override
+    public String getPropertyOptions() {
+        String encryption = "";
+        if (SecurityUtil.getDataEncryption() != null) {
+            encryption = ",{name : 'encryption', label : '@@form.textfield.encryption@@', type : 'checkbox', value : 'false', ";
+            encryption += "options : [{value : 'true', label : '' }]}";
+        }
+        
+        return AppUtil.readPluginResource(getClass().getName(), "/properties/form/textFieldMasking.json", new Object[]{encryption}, true, "messages/form/TextFieldMasking");
+    }
+
+    @Override
+    public String getFormBuilderCategory() {
+        return "Marketplace";
+    }
+
+    @Override
+    public int getFormBuilderPosition() {
+        return 500;
+    }
+
+    @Override
+    public String getFormBuilderIcon() {
+        return "<i class=\"fas fa-info\"></i>";
+    }
+
+}
